3e4e8a7e79bda633e5273ddf2b4a196b81ce2b89
Hazuan Sarah <hazuansarah@Hazuans-MacBook-Pro.local>
2026-04-04 09:12:19 -0700

    setup local database connection

---
+++ backend/configs/database.js
@@ -0,0 +1,24 @@
+import "dotenv/config";
+import { Sequelize } from "sequelize";
+
+const {
+  DATABASE_HOST,
+  DATABASE_NAME,
+  DATABASE_USER,
+  DATABASE_PASSWORD,
+} = process.env;
+
+const config = {
+    host: DATABASE_HOST,
+    dialect: "postgres",
+    logging: false,
+    pool: {
+        max: 15,
+        min: 0,
+        idle: 10000,
+    },
+};
+
+const sequelize = new Sequelize(DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD, config);
+
+export default sequelize;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import sequelize from './configs/database.js';
 
 dotenv.config();
 
@@ -19,4 +20,12 @@ app.get('/api/server-health', (req, res) => {
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  try {
+    sequelize.authenticate()
+    .then(() => {
+      console.log('Connected to the database.');
+    })
+  } catch (error) {
+    console.error('Error connecting to the database:', error);
+  }
 });
